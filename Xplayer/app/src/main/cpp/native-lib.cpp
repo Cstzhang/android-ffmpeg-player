569a06d8424bb544600952d29be47b37ec0714a1
Cstzhangzb <devzhangzb@163.com>
2018-09-25 16:27:35 +0800

    视频FPS

--- Xplayer/app/src/main/cpp/native-lib.cpp
+++ Xplayer/app/src/main/cpp/native-lib.cpp
@@ -47,7 +47,7 @@ Java_xplayer_xplayer_MainActivity_stringFromJNI(
     //封装格式上下文
     AVFormatContext *ic = NULL;
 
-    char path[] = "/sdcard/testFFmpeg.mp4";
+    char path[] = "/sdcard/cat.mp4";
     /*
       • AVFormatContext **ps  传指针的地址
       • const char *url   文件路径（本地的或者网络的http rtsp 地址会被存在AVFormatContext 结构体的 fileName中）
@@ -86,19 +86,19 @@ Java_xplayer_xplayer_MainActivity_stringFromJNI(
             videoStream = i;
             fps = r2d(as->avg_frame_rate);
 
-            LOGW("fps = %d  width = %d  height = %d  codeid = %d   pixformat %d ",fps,
-                 as->codecpar->width,
-                 as->codecpar->height,
-                 as->codecpar->codec_id,
-                 as->codecpar->format);
+//            LOGW("fps = %d  width = %d  height = %d  codeid = %d   pixformat %d ",fps,
+//                 as->codecpar->width,
+//                 as->codecpar->height,
+//                 as->codecpar->codec_id,
+//                 as->codecpar->format);
 
         }else if (as->codecpar->codec_type == AVMEDIA_TYPE_AUDIO){
             LOGW("音频数据");
             audioStream = i;
-            LOGW("sample_rate %d  channels =%d sample_format= %d",
-            as->codecpar->sample_rate,
-            as->codecpar->channels,
-            as->codecpar->format);
+//            LOGW("sample_rate %d  channels =%d sample_format= %d",
+//            as->codecpar->sample_rate,
+//            as->codecpar->channels,
+//            as->codecpar->format);
         }
 
     }
@@ -130,52 +130,52 @@ Java_xplayer_xplayer_MainActivity_stringFromJNI(
         return  env->NewStringUTF(hello.c_str());
     }
     //解码器初始化
-    AVCodecContext *cc = avcodec_alloc_context3(codec);
-    avcodec_parameters_to_context(cc,ic->streams[videoStream]->codecpar);
-    cc->thread_count = 1;
+    AVCodecContext *vc = avcodec_alloc_context3(codec);
+    avcodec_parameters_to_context(vc,ic->streams[videoStream]->codecpar);
+    vc->thread_count = 4;
 
     //打开解码器
-    re = avcodec_open2(cc,0,0);
+    re = avcodec_open2(vc,0,0);
     if (re != 0) {
         LOGW("avcodec_open2 videoStream fail!");
         return  env->NewStringUTF(hello.c_str());
     }
 
-//    //音频解码器
-//    //软解码
-//    AVCodec *acodec = avcodec_find_decoder(ic->streams[audioStream]->codecpar->codec_id);
-//    //硬解码
-////        acodec = avcodec_find_decoder_by_name("h264");
-//    if(!acodec) {
-//        LOGW("avcodec_find_decoder audioStream fail!");
-//        return  env->NewStringUTF(hello.c_str());
-//    }
-//    //解码器初始化
-//    AVCodecContext *ac = avcodec_alloc_context3(acodec);
-//    avcodec_parameters_to_context(ac,ic->streams[audioStream]->codecpar);
-//    ac->thread_count = 1;
-//
-//    //打开解码器
-//    re = avcodec_open2(ac,0,0);
-//    if (re != 0) {
-//        LOGW("avcodec_open2 audioStream fail!");
-//        return  env->NewStringUTF(hello.c_str());
-//    }
+    //音频解码器
+    //软解码
+    AVCodec *acodec = avcodec_find_decoder(ic->streams[audioStream]->codecpar->codec_id);
+    //硬解码
+//        acodec = avcodec_find_decoder_by_name("h264");
+    if(!acodec) {
+        LOGW("avcodec_find_decoder audioStream fail!");
+        return  env->NewStringUTF(hello.c_str());
+    }
+    //解码器初始化
+    AVCodecContext *ac = avcodec_alloc_context3(acodec);
+    avcodec_parameters_to_context(ac,ic->streams[audioStream]->codecpar);
+    ac->thread_count = 4;
+
+    //打开解码器
+    re = avcodec_open2(ac,0,0);
+    if (re != 0) {
+        LOGW("avcodec_open2 audioStream fail!");
+        return  env->NewStringUTF(hello.c_str());
+    }
 
     //read ic
     AVPacket *pkt = av_packet_alloc();//create and init
-//    AVFrame  *frame = av_frame_alloc();
-//    long long start = getNowMs();
-//    int frameCount = 0;
+    AVFrame  *frame = av_frame_alloc();
+    long long start = getNowMs();
+    int frameCount = 0;
     for (;;)
     {
 
-        //当时间超过4秒
-//        if (getNowMs() - start >= 3000) {
-//            LOGW("now decode fps is %d",frameCount/3);
-//            start = getNowMs();
-//            frameCount = 0;
-//        }
+        //当时间超过3秒
+        if (getNowMs() - start >= 3000) {
+            LOGW("now decode fps is %d",frameCount/3);//平均每秒解多少帧
+            start = getNowMs();
+            frameCount = 0;
+        }
         int re = av_read_frame(ic,pkt);
 
         if (re != 0){
@@ -184,47 +184,42 @@ Java_xplayer_xplayer_MainActivity_stringFromJNI(
             av_seek_frame(ic,videoStream,pos,AVSEEK_FLAG_BACKWARD|AVSEEK_FLAG_FRAME);
             continue;
         }
-        LOGW("stream = %d size =%d pts=%lld flag=%d",
-             pkt->stream_index,pkt->size,pkt->pts,pkt->flags
-        );
-//        LOGW("stream = %d size =%d pts =%lld flag =%d",
-//             pkt->stream_index,pkt->size,pkt->pts,pkt->flags);
+//        LOGW("stream = %d size =%d pts=%lld flag=%d",
+//             pkt->stream_index,pkt->size,pkt->pts,pkt->flags
+//        );
 
-//        //测试音视频解码
-//        AVCodecContext *cc = vc;
-//        if (pkt->stream_index == audioStream) {//判断是音频
-//            cc = ac;
-//        }
-//
-//        //发送到线程汇总解码
-//        re = avcodec_send_packet(cc,pkt);
-//
-//        //销毁
-//        av_packet_unref(pkt);
-//
-//        if  (re != 0){
-//            LOGW("avcodec_send_packet audio failed!");
+//        if (pkt->stream_index != videoStream){
 //            continue;
 //        }
-//
-//        for (; ;) {//保证能搜到全部的解码数据
-//            re = avcodec_receive_frame(cc,frame);
-//            if (re != 0) {
-//                char buff[1024] = {0};
-//                av_strerror(re, buff, sizeof(buff));
-//                LOGW("avcodec_receive_frame failed! %s",buff);
-//                break;
-//            }
-//            if (cc == vc) {
-//                frameCount++;
-//            }
-//
-//        }
+
+        AVCodecContext *cc = vc;
+        if (pkt->stream_index == audioStream){
+            cc = ac;
+        }
+
+
+        //发送到线程汇总解码
+        re = avcodec_send_packet(cc,pkt);
+
         av_packet_unref(pkt);
+        if (re != 0){
+            LOGW("avcodec_send_packet faild!");
+            continue;
+        }
+        for (;;) {
 
-//        LOGW(" avcodec_receive_frame  %lld",frame->pts);
+        re = avcodec_receive_frame(cc,frame);
+         if (re != 0){
+             //LOGW("avcodec_receive_frame faild!");
+             break;
+         }
+           // LOGW("avcodec_receive_frame %lld",frame->pts);
+            if (cc == vc){//如果是视频帧
+                frameCount++;
+            }
 
-
+        }
+        av_packet_unref(pkt);
 
     }
 
